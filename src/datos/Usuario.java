8df444f67edcc039eb2aad796289b7eefb504f24
Lisandro Canessa <lisandrocanessa@hotmail.com>
2019-07-06 14:31:30 -0300

    Removidos unused imports

--- src/datos/Usuario.java
+++ src/datos/Usuario.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Usuario {
